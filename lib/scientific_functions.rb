a8da913e46870c13b3a560f893070d9944aa78be
Matthew Wilson <msw@mwilson.localdomain>
2010-10-27 14:13:07 -0400

    Initial Commit

---
+++ lib/scientific_functions.rb
@@ -0,0 +1,15 @@
+# ScientificFunctions
+class String
+  def sigfigs(num_places)
+    value = BigDecimal(self).split
+    result = value[1].to_i.round(num_places - value[1].length).to_s
+    result = (".#{result}".to_d * (10.to_d**value[3])).to_s
+    if value[0] == -1
+      result = "-" << result
+    end
+    if value[3] > 0
+      result.gsub!(".0","")
+    end
+    return result
+  end
+end
