c3a2817bee650e6653f71bb191af9d9cfed54f46
mwilson <mattwilsoncp@suddenlink.net>
2010-11-04 15:00:12 -0400

    Complete rewrite of function

--- lib/scientific_functions.rb
+++ lib/scientific_functions.rb
@@ -1,19 +1,40 @@
 # ScientificFunctions
 class String
   def sigfigs(num_places)
+    result = String.new
     value = BigDecimal(self).split
-    result = value[1].to_i.round(num_places - value[1].length).to_s
-    result = (".#{result}".to_d * (10.to_d**value[3])).to_s
+    if num_places > value[1].length
+      result = value[1].ljust(num_places, "0")
+    else
+      result = value[1].to_i.round(num_places - value[1].length).to_s.slice(0, num_places).to_i.to_s
+      if num_places >= value[1].length
+        result = result.ljust(value[1].length,"0")
+      end
+    end
+
+    if value[3] < 0
+      result = "0." << "0".rjust(value[3].abs,"0") << result
+    end
+    if value[3] == 0
+      result = "0." << result
+    end
+    if value[3] > 0
+      p1 = result.slice(0, value[3])
+      if p1.length < value[3]
+        p1 = p1.ljust(value[3],"0")
+      end
+      if p1.length < num_places
+        result = p1 << "." << result.slice(value[3], result.length)
+      else
+        result = p1
+      end
+    end
+    
     if value[0] == -1
       result = "-" << result
     end
-    if value[3] > 0
-      result.gsub!(".0","")
-    end
-    if (result.length < num_places) && (result.to_f >= 1) && (result.index(".").nil?)
-      result = result << "."
-      result = result.ljust(num_places + 1, "0")
-    end
-    return result
+    return result    
   end
 end
+
+
